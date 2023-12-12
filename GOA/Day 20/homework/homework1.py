c8638b882fc86b11209a2d71e75998a9d2fcee20
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2023-12-12 22:34:21 +0400

    Add files via upload

---
+++ GOA/Day 20/homework/homework1.py
@@ -0,0 +1,3 @@
+# Find the needle in a list
+def find_needle(haystack):
+    return "found the needle at position " + str(haystack.index("needle"))
---
+++ GOA/Day 20/homework/homework3.py
@@ -0,0 +1,7 @@
+# Are you playing banjo?
+def are_you_playing_banjo(name):
+    if name[0] == "R" or name[0] == "r":
+        return name + " plays banjo"
+    else:
+        return name + " does not play banjo"
+print("Do you play banjo?")

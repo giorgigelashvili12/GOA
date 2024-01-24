e32462fae77975d1662e2e793f0db252c38e3042
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2024-01-24 20:49:05 +0400

    Day 2

---
+++ Day 2/homework/index.py
@@ -0,0 +1,8 @@
+mothers_age = 39
+fathers_age = 39
+grandmothers_age = 60
+grandfathers_age = 62
+my_age = 12
+future = 10
+
+print(fathers_age - (my_age + future))
---
+++ Day 7/homework.py
@@ -0,0 +1,7 @@
+ticket_price = 25 #lari
+child_ticket = 0 #free
+old_people = 10
+young_people = 3
+sum = ((ticket_price * old_people) + (young_people * 3))
+
+print(sum)

3243763f9c3c621b1b0124a7698083f6b4940dd9
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2024-04-26 21:25:29 +0400

    d

--- Day 56/Classwork/script.js
+++ Day 56/Classwork/script.js
@@ -24,3 +24,43 @@ function grow(x){
 } // ver gavige
   
 // 4
+function fakeBin(x) {
+  let chars = [];
+
+  for (let i = 0; i < x.length; i++) {
+    let num = parseInt(x[i]);
+    
+    if (num < 5) {
+      chars.push('0');
+    } else {
+      chars.push('1');
+    }
+  }
+  
+  let result = chars.join('');
+  
+  return result;
+}
+
+// 5
+function countBy(x, n) {
+  let result = [];
+  for(let i = 1; i <= n; i++) {
+    result.push(x * i)
+  }
+  return result;
+}
+// gaketebuli mqonda
+
+// 6
+String.prototype.toJadenCase = function () {
+  let wordCount = this.split();
+  let casedWords = (function(word) {
+    return word[0].toUpperCase() + word.slice(1);
+  });
+    
+  return casedWords.join(' ');
+};
+// es vergavige
+
+// 7

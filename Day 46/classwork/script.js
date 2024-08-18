a2716c6becbd0ee660147e21799d77be9af05d5c
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2024-08-18 16:59:49 +0400

    hw

---
+++ Day 46/classwork/script.js
@@ -0,0 +1,9 @@
+console.log('My first name is Giorgi');
+console.log('My last name is Gelashvili');
+
+console.log(2 + 2);
+console.log(50 / 2);
+console.log(60 * 1);
+console.log(1 - 100);
+
+alert('Hello');

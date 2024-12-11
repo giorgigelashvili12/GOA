67ca984537d6532dda0f6699f96fcff992f6f000
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2024-12-11 21:49:08 +0400

    GOA

---
+++ Day 128/Classwork/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Routes, Router } from 'react-router';
+import Home from './pages/Home.jsx';
+import Books from './pages/Books.jsx';
+
+export default function App() {
+  const navList = [
+    {
+      path: '/',
+      name: 'Home Page'
+    },
+    {
+      path: '/books',
+      name: 'Books Page'
+    }
+  ];
+
+  return (
+      <div>
+        {navList.map((item, index) => {
+          <li key={`_navlinks_${index}`}><Link to={item.path}>{item.name}</Link></li>
+        })}
+
+        <Routes>
+         <Router path='/' element={<Home />}></Router>
+         <Router path='/books' element={<Books />}></Router>
+        </Routes>
+      </div>
+  )
+}

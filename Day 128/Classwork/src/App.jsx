50ba8acaf2176fe8df0c75348a3d11c95c26c218
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2024-12-11 22:25:00 +0400

    GOA

--- Day 128/Classwork/src/App.jsx
+++ Day 128/Classwork/src/App.jsx
@@ -12,6 +12,14 @@ export default function App() {
     {
       path: '/books',
       name: 'Books Page'
+    },
+    {
+      path: '/books/modules',
+      name: 'Book Modules :-)'
+    }, 
+    {
+      path: '/books/new/page',
+      name: 'New Books Page'
     }
   ];
 
@@ -24,6 +32,8 @@ export default function App() {
         <Routes>
          <Router path='/' element={<Home />}></Router>
          <Router path='/books' element={<Books />}></Router>
+         <Router path='/books/modules' element={<BooksModules />}></Router>
+         <Router path='/books/new/page' element={<BooksNewPage />}></Router>
         </Routes>
       </div>
   )

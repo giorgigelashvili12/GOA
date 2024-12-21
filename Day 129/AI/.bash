6ab3110c87b01336696cd9d06a470ceed6a6fb58
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2024-12-21 22:09:02 +0400

    GOA

---
+++ Day 129/AI/.bash
@@ -0,0 +1,10 @@
+$ ls -lh
+$ kate main.c
+$ Makefile 2>/dev/null
+$ make
+$ ls -lh
+$ make debug
+# Opens a window, rule debug in makefile, ru
+# After running
+(gdb) winheight src -5 # minimizes src window
+(gdb)

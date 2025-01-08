2b6e54ccc0ca7b7272eb9f525d878c05d8561bf2
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2025-01-08 22:24:09 +0400

    go

---
+++ Day 140/Classwork/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+  <>
+    <h1 className="text-3xl font-bold underline text-emerald-500 border-2 border-emerald-400 p-10 m-20">
+      React is confusing, bakcend easier
+    </h1>
+    <button className="text-emerald-700 m-10 p-5 border-2 border-emerald-400 ra">Ofc bro that right</button>
+    <button className="text-[#ef4444] m-10 p-5 border-2 border-emerald-400 ra">No bro wat, react is ez</button>
+  </>  
+  )
+}

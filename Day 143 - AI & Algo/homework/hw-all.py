6007855108cf162015fa6f2e179e82d57515809f
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2025-01-23 18:50:11 +0400

    AI & Algorithmics

--- Day 143 - AI & Algo/homework/hw-all.py
+++ Day 143 - AI & Algo/homework/hw-all.py
@@ -23,6 +23,31 @@ b(1, 99, 3)
 # def f():
 #     return
     # პოზიციური სისტემა ანუ bin, hex?
+def f(n, b):
+    # monacemis gadamowmeba
+    if n == 0:
+        return "0"
+    
+    negative = n < 0
+
+    if negative:
+        n = abs(n)
+
+    v = []
+    #monacemis damushaveba
+    r = n % b
+    v.append(str(r))
+
+    v.reverse()
+    result = ''.join(v)
+
+    if negative: 
+        result = '-' + result 
+    return result
+
+print(f(45, 3))
+print(f(25, 6))
+print(f(-10, 3))
 
 # HW 3
 # გამოიკვლიეთ მასივში მაქსიმუმის მოძებნის ამოცანა 5 ელემენტის შემთხვევაში

4ddea7431799306b9efd097be4f7587cdd5417aa
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2025-05-22 22:15:24 +0400

    Homework

---
+++ Day 167/homework/src/App.tsx
@@ -0,0 +1,14 @@
+export default function App() {
+    return (
+            <div className="w-screen h-screen bg-blue-200 flex justify-center items-center">
+                <div className="w-120 h-150 bg-white rounded-2xl flex flex-col justify-center items-center gap-2">
+                    <h1 className="font-bold text-2xl">Sign in with</h1>
+                    <div className="flex justify-center items-center gap-2">
+                        <div className="p-3 border-1 border-gray-500 rounded">FB</div>
+                        <div className="p-3 border-1 border-gray-500 rounded">AP</div>
+                        <div className="p-3 border-1 border-gray-500 rounded">GO</div>
+                    </div>
+                </div>
+             </div>
+    )
+}

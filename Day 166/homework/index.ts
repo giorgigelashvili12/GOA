6fbe63901c21bf3bbfda196381b499cace105a46
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2025-05-26 22:23:21 +0400

    g

---
+++ Day 166/homework/index.ts
@@ -0,0 +1,50 @@
+interface User {
+    id: number;
+    username: string;
+    email: string;
+    isAdmin?: boolean;
+}
+
+function returnAdmins(users: User[]): User[] {
+    return users.filter(user => user.isAdmin);
+}
+
+function getFirstItem<T>(arr: T[]): T {
+    return arr[0];
+}
+
+const baba = getFirstItem<string>(["hello", "world"]);
+
+type ApiResponse = {
+    status: "success";
+    data: {
+        id: number;
+        name: string;
+    };
+};
+
+function getName(response: ApiResponse): string {
+    return response.data.name;
+}
+
+enum Role {
+    Guest,
+    User,
+    Admin,
+}
+
+function welcome(role: Role): string {
+    if (role === Role.Guest) {
+        return "Guest access only";
+    }
+      
+    if (role === Role.User) {
+        return "Welcome User!";
+    }
+      
+    if (role === Role.Admin) {
+        return "Welcome Admin!";
+    }
+      
+    return "Unknown role";
+}
---
+++ Day 168/homework/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div className="w-screen h-screen bg-blue-200 flex justify-center items-center w-full">
+            <div className="w-120 h-150 bg-white rounded-2xl">
+
+            </div>
+        </div>
+    )
+}
